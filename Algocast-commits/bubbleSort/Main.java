b3148f5d96e1d6601511324b5298661dfa1be3e7
Ssegura09 <stephaniemsegura09@gmail.com>
2021-05-10 17:06:00 -0500

    bubble sort

--- Algocast-commits/bubbleSort/Main.java
+++ Algocast-commits/bubbleSort/Main.java
@@ -1,35 +1,35 @@
-package academy.learnprogramming.bubblesort;
+// package academy.learnprogramming.bubblesort;
 
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+//         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(intArray, i, i + 1);
-                }
-            }
-        }
+//         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+//             for (int i = 0; i < lastUnsortedIndex; i++) {
+//                 if (intArray[i] > intArray[i + 1]) {
+//                     swap(intArray, i, i + 1);
+//                 }
+//             }
+//         }
 
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
-        }
+//         for (int i = 0; i < intArray.length; i++) {
+//             System.out.println(intArray[i]);
+//         }
 
 
-    }
+//     }
 
-    public static void swap(int[] array, int i, int j) {
+//     public static void swap(int[] array, int i, int j) {
 
-        if (i == j) {
-            return;
-        }
+//         if (i == j) {
+//             return;
+//         }
 
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+//         int temp = array[i];
+//         array[i] = array[j];
+//         array[j] = temp;
 
-    }
-}
+//     }
+// }
